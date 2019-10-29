1cb4c827a50186f97c4f35562ee80cb0d6372ebb
PedroHRPBS <pedro.silva@ku.ac.ae>
2019-10-29 10:37:40 +0400

    bug fix: payload conversion

--- src/ROSUnit_Positioning.cpp
+++ src/ROSUnit_Positioning.cpp
@@ -20,21 +20,26 @@ void ROSUnit_Positioning::setSubscribers(){
 
 void ROSUnit_Positioning::callbackPositioning(const geometry_msgs::PoseStamped& msg){
 
-    //std::cout << msg;
+    std::cout << msg.pose.position.x << std::endl;
     Vector3D _posXYZ;
-    //_msg_type = msg_type::position;
+ 
+    double tmp_double[3];
+    tmp_double[0] = msg.pose.position.x;
+    tmp_double[1] = msg.pose.position.y;
+    tmp_double[2] = msg.pose.position.z;
 
-    double tmp_float = msg.pose.position.x;
-    uint8_t* data_ptr=(uint8_t*)&tmp_float;
-    uint8_t payload[3];
-    payload[0]=*((uint8_t*)data_ptr++);
-    payload[1]=*((uint8_t*)data_ptr++);
-    payload[2]=*((uint8_t*)data_ptr++);
+    uint8_t* data_ptr=(uint8_t*)&tmp_double[0];
+    uint8_t payload[sizeof(double) * 3];
 
+    for(int i = 0; i < sizeof(double) * 3; i++){
+        payload[i]=*((uint8_t*)data_ptr++);
+    }
+    
     std::cout << payload <<std::endl;
 
     //Workaround for accessing msg_type inside a static function
     instance_ptr->emit_message(payload, 3, msg_type::position);
+    
 }
 
 void ROSUnit_Positioning::receive_msg_data(uint8_t data[], std::size_t len, msg_type _msg_type){
