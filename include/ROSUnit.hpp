742e8261f56633929f82b23d7cfad71a5ee99c7d
PedroHRPBS <pedro.silva@ku.ac.ae>
2019-10-30 09:51:32 +0400

    docs: Typos

--- include/ROSUnit.hpp
+++ include/ROSUnit.hpp
@@ -17,7 +17,7 @@ class ROSUnit : public msg_emitter, public msg_receiver{
         virtual void setSubscribers() = 0;
         virtual void receive_msg_data(uint8_t data[],std::size_t len, msg_type _msg_type) = 0;
         //template<typename T>
-        void serializeData(double data[], int len, uint8_t wtv[]); // member template
+        void serializeData(double data[], int numVariables, uint8_t serializedData[]); // member template
         
         ROSUnit(ros::NodeHandle& _main_handler);
         ROSUnit();
