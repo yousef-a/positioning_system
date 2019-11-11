ddc03df6b8460154ecf9244cd45d2409b64eb78f
PedroHRPBS <pedro.silva@ku.ac.ae>
2019-11-11 12:10:23 +0400

    feat: implent PID_Controller class

---
+++ include/PID_Controller.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "Controller.hpp"
+
+class PID_Controller : public Controller{
+
+public:
+    PID_Controller();
+    ~PID_Controller();
+};
---
+++ src/PID_Controller.cpp
@@ -0,0 +1,9 @@
+#include "PID_Controller.hpp"
+
+PID_Controller::PID_Controller() {
+
+}
+
+PID_Controller::~PID_Controller() {
+
+}
