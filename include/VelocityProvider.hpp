d1b59f6ac9d2ae43ec175c72deb1c551eaaef53d
PedroHRPBS <pedro.silva@ku.ac.ae>
2019-12-11 14:26:04 +0400

    Add VelocityMsg

--- include/VelocityProvider.hpp
+++ include/VelocityProvider.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "Provider.hpp"
+#include "VelocityMsg.hpp"
 
 class VelocityProvider : public Provider{
 
 public:
 
+    virtual VelocityMsg geteVelocity() = 0;
     DataMessage* receive_msg_internal(DataMessage*);
     DataMessage* receive_msg_internal();
     void receive_msg_data(DataMessage* t_msg);
