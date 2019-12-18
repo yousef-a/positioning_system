2e584057e8e2927941d3aa2e724ab5cbc0676701
PedroHRPBS <pedro.silva@ku.ac.ae>
2019-12-18 17:30:28 +0400

    NAVIO test

--- src/Roll_PVProvider.cpp
+++ src/Roll_PVProvider.cpp
@@ -10,8 +10,10 @@ Roll_PVProvider::~Roll_PVProvider() {
 
 Vector3D<float> Roll_PVProvider::getProcessVariable(){
 
+    AttitudeMsg att = this->getAttitude();
+
     Vector3D<float> t_process_variable;
-    t_process_variable.x = this->getAttitude().roll;
+    t_process_variable.x = att.roll;
     if(this->getBodyRateProvider()){
         t_process_variable.y = (this->getBodyRateProvider()->getBodyRate().y) * (M_PI/180);
     }else{
@@ -22,7 +24,7 @@ Vector3D<float> Roll_PVProvider::getProcessVariable(){
     ros_msg.setRoll_PV(t_process_variable);
     this->PVProvider::emit_message((DataMessage*) &ros_msg);
 
-    ros_msg.setAttitude(this->getAttitude());
+    ros_msg.setAttitude(att);
     this->AttitudeProvider::emit_message((DataMessage*) &ros_msg);
 
     return t_process_variable;
